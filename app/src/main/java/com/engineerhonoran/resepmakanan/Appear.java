48c373302224813b147110ab8c989b69de246fff
henryaugusta <henryaugusta4@gmail.com>
2019-12-08 14:29:24 +0700

    Tamabah Class Appear

---
+++ app/src/main/java/com/engineerhonoran/resepmakanan/Appear.java
@@ -0,0 +1,14 @@
+package com.engineerhonoran.resepmakanan;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Appear extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_appear);
+    }
+}
--- app/src/main/java/com/engineerhonoran/resepmakanan/Minuman.java
+++ app/src/main/java/com/engineerhonoran/resepmakanan/Minuman.java
@@ -1,14 +1,34 @@
 package com.engineerhonoran.resepmakanan;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Minuman extends AppCompatActivity {
-
+    Button tombolcendol;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_minuman);
+
+
+        tombolcendol.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent pindah=new Intent(Minuman.this,Appear.class);
+                Bundle datanya=new Bundle();
+                datanya.put
+            }
+        });
+
+    }
+    private void init(){
+
+        tombolcendol = (Button) findViewById(R.id.btn_cendol);
     }
 }
